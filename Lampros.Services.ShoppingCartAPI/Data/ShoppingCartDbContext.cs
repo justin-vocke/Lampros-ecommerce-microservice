1733e2d15321a3ba36c6caf87e12d1e4651a15d3
Justin Vocke <therealmathhelper@gmail.com>
2024-07-20 12:52:55 -0400

    Add authn, DbContext, initial migration, and models/DTOs.

---
+++ Lampros.Services.ShoppingCartAPI/Data/ShoppingCartDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+using Lampros.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lampros.Services.ShoppingCartAPI.Data
+{
+    public class ShoppingCartDbContext: DbContext
+    {
+        public ShoppingCartDbContext(DbContextOptions<ShoppingCartDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+
+        public DbSet<CartDetails> CartDetails { get; set; }
+        public DbSet<CartHeader> CartHeader { get; set; }   
+    }
+}
---
+++ Lampros.Services.ShoppingCartAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Lampros.Services.ShoppingCartAPI.Models.Dto
+{
+    public record CouponDto(int CouponId, string CouponCode, double DiscountAmount, int? MinAmount);
+    
+}
