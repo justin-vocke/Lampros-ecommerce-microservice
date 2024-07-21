89cdf57af4b04143dac999aee87b91e57a1a706f
Justin Vocke <therealmathhelper@gmail.com>
2024-07-20 20:38:57 -0400

    Add models/services/ and views for shopping cart to MVC project.

---
+++ Lampros.MVC/Models/CartDto.cs
@@ -0,0 +1,10 @@
+﻿using Lampros.MVC.Models.Dto;
+
+namespace Lampros.MVC.Models
+{
+    public class CartDto
+    {
+        public CartHeaderDto CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto?> CartDetails { get; set; }
+    }
+}
